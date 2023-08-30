d5c42329cb2afc3b7673404cdc603c84290f8d82
MannemSaivarun <mannemsaivarun992@gmail.com>
2023-08-30 15:39:30 +0530

    server is created

--- first.js
+++ first.js
@@ -1,28 +1,38 @@
-console.log("hello world");
+// console.log("hello world");
 
-let product = (a,b) =>{
-    return a*b
-}
-console.log(product(2,3))
+// let product = (a,b) =>{
+//     return a*b
+// }
+// console.log(product(2,3))
 
-const person = {
-    name:"Mark",
-    age:20,
-    greet: function(){
-        console.log("hello I am "+this.name+"my age is "+this.age)
-    }
+// const person = {
+//     name:"Mark",
+//     age:20,
+//     greet: function(){
+//         console.log("hello I am "+this.name+"my age is "+this.age)
+//     }
 
-}
-person.greet()
+// }
+// person.greet()
 
-const array =['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']
+// const array =['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']
 
 
-console.log(array.map((num)=>{
-    if(num==" "){
-        return "empty string"
-    }else{
-        return num
-    }
-}))
+// console.log(array.map((num)=>{
+//     if(num==" "){
+//         return "empty string"
+//     }else{
+//         return num
+//     }
+// }))
 
+
+const http = require('http');
+const server = http.createServer((request,response)=>{
+    response.writeHead("200");
+    response.end("hi my name is varun")
+})
+
+server.listen(4000,"localhost",()=>{
+    console.log("hi my name varun")
+})
